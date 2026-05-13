c031520828449391e5e08e3ec942ad43a5fa1046
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2026-05-13 19:36:28 -0400

    Use no-cache header to make client verify with server before using cached index.html

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Tells the browser to check with server before using its cached version
+		w.Header().Set("Cache-Control", "no-cache, must-revalidate")
 		http.ServeFile(w, r, "static/index.html")
 	})
 
